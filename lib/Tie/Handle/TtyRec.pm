48c906bcc4676e5a311232f744aa00995345d67a
Shawn M Moore <sartak@gmail.com>
2012-03-06 11:55:44 -0500

    Copyright

--- lib/Tie/Handle/TtyRec.pm
+++ lib/Tie/Handle/TtyRec.pm
@@ -100,7 +100,7 @@ Shawn M Moore, C<sartak@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-2009 Shawn M Moore.
+Copyright 2007-2012 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
